42af68859d690c9e3d947c437a97e883ce7219ce
Murray Jack <mjack@starrez.com>
2019-09-09 15:22:32 +1000

    added sentry io

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Projects from "../components/projects";
+import Projects from "../components/projects"
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({ dsn: 'https://539f971cbec34ef59994fb40fb5b122c@sentry.io/1686105' });
 
 const IndexPage = () => (
   <Layout>
